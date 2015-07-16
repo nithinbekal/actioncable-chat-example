7d32abeb70dada392f7d1f3644517ad8a073df3e
Nithin Bekal <nithinbekal@gmail.com>
2015-07-16 17:45:05 +0530

    Show username along with message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,8 +4,13 @@ class MessagesController < ApplicationController
 
   def create
     ActionCable.server.broadcast 'messages',
-      message: MessagesController.render(partial: 'messages/message',
-                                         locals: { message: params[:message][:body] })
+      message: MessagesController.render(
+        partial: 'messages/message',
+        locals: {
+          message: params[:message][:body],
+          username: cookies.signed[:username]
+        }
+      )
     head :ok
   end
 end
