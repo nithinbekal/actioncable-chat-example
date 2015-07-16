af69d0d19dd116282e0fb470f3ad452f3b55b25e
Nithin Bekal <nithinbekal@gmail.com>
2015-07-16 17:26:16 +0530

    Remove extra config in ApplicationCable::Connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :username
-
-    def connect
-      self.username = cookies.signed[:username]
-    end
   end
 end
