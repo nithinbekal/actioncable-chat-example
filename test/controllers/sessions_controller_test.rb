4b065bcc811f1930d88b35f20527dd10a32ca6d3
Keenan Brock <keenan@thebrocks.net>
2015-10-18 13:20:55 -0400

    properly pass params to tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   def test_setting_username
-    post :create, session: { username: 'john' }
-    assert_equal 'john', cookies.signed[:username]
+    post :create, params: { session: { username: 'john' } }
+    assert_equal 'john', request.cookie_jar.signed[:username]
     assert_redirected_to messages_path
   end
 end
